ba8d820796b1401fbefcfbd33682cb091eb2cd2f
alex <sanatateacom@gmail.com>
2020-10-05 19:58:25 +0300

    updated product model and addProductRequest

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,8 +8,8 @@ class Product extends Model
 {
     protected $fillable = ['title', 'description', 'price'];
 
-//    public function categories()
-//    {
-//        return $this->belongsToMany('App\Category');
-//    }
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
 }
