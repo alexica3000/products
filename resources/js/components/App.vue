6a6a3c3c7c9697f8223e311bf7499d2c62d024ed
alex <sanatateacom@gmail.com>
2020-10-06 21:05:39 +0300

    created navbar component

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,9 +1,39 @@
 <template>
     <div>
-        <example-component></example-component>
+        <Navbar/>
+        <main class="py-4">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-10">
+
+                    </div>
+                </div>
+            </div>
+        </main>
     </div>
 </template>
 
 <style>
 
 </style>
+
+<script>
+    import Navbar from "./Navbar";
+
+    export default {
+        components: {
+            Navbar
+        },
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+
+        },
+        created() {
+
+        }
+    }
+</script>
