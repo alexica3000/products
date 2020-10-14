49ea7a21d40f7836789cfc1d24caa3da06fdb0e7
alex <sanatateacom@gmail.com>
2020-10-14 12:39:51 +0300

    added loader for products list

--- resources/js/pages/product/ProductList.vue
+++ resources/js/pages/product/ProductList.vue
@@ -1,52 +1,54 @@
 <template>
     <div class="card">
+            <div class="card-header">
+                <div class="row">
+                    <div class="col">
+                        <h3>Products</h3>
+                    </div>
 
-        <div class="card-header">
-            <div class="row">
-                <div class="col">
-                    <h3>Products</h3>
+    <!--                @if (Auth::check())-->
+    <!--                <div class="col text-right">-->
+    <!--                    <a class="btn btn-primary" href="{{ route('product.create') }}">New product</a>-->
+    <!--                </div>-->
+    <!--                @endif-->
                 </div>
-
-<!--                @if (Auth::check())-->
-<!--                <div class="col text-right">-->
-<!--                    <a class="btn btn-primary" href="{{ route('product.create') }}">New product</a>-->
-<!--                </div>-->
-<!--                @endif-->
             </div>
-        </div>
 
-        <div class="card-body">
+            <div class="spinner-border" role="status" v-if="loadingComponent">
+                <span class="sr-only">Loading...</span>
+            </div>
 
-            <table class="table">
-                <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Product</th>
-                    <th scope="col">Description</th>
-                    <th scope="col">Price</th>
-                    <th scope="col">Categories</th>
-                    <th scope="col">Actions</th>
-                </tr>
-                </thead>
-                <tbody>
-                    <tr scope="row" v-for="(product, index) in products">
-                        <th>{{ index + 1 }}</th>
-                        <td>
-<!--                            <a href="{{ route('product.show', $product->id) }}">{{ $product->title }}</a>-->
-                        </td>
-                        <td>{{ product.description }}</td>
-                        <td>{{ product.price }}</td>
-                        <td>
-<!--                            @foreach ($product->categories as $category)-->
-<!--                            <div><a href="{{ route('cat.product', $category->id) }}">{{ $category->title }}</a></div>-->
-<!--                            @endforeach-->
-                        </td>
-                        <td>Edit | Delete</td>
+            <div class="card-body" v-else>
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Product</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Categories</th>
+                        <th scope="col">Actions</th>
                     </tr>
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        <tr scope="row" v-for="(product, index) in products">
+                            <th>{{ index + 1 }}</th>
+                            <td>
+    <!--                            <a href="{{ route('product.show', $product->id) }}">{{ $product->title }}</a>-->
+                            </td>
+                            <td>{{ product.description }}</td>
+                            <td>{{ product.price }}</td>
+                            <td>
+    <!--                            @foreach ($product->categories as $category)-->
+    <!--                            <div><a href="{{ route('cat.product', $category->id) }}">{{ $category->title }}</a></div>-->
+    <!--                            @endforeach-->
+                            </td>
+                            <td>Edit | Delete</td>
+                        </tr>
+                    </tbody>
+                </table>
 
-        </div>
+            </div>
     </div>
 </template>
 
@@ -62,6 +64,8 @@
         },
         methods: {
             async retrieveProducts() {
+                this.loadingComponent = true;
+
                 await ProductDataService.getAll()
                     .then(r => {
                         this.products = r.data.data;
@@ -78,6 +82,8 @@
     }
 </script>
 
-<style>
-
+<style scoped>
+    .spinner-border {
+        margin: 30px;
+    }
 </style>
