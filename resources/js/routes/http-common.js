16fe286ef4fab74f4ef384889b47d753480a4b3f
alex <sanatateacom@gmail.com>
2020-10-16 17:21:53 +0300

    removed vuejs files

--- resources/js/routes/http-common.js
+++
@@ -1,9 +0,0 @@
-import Axios from "axios";
-
-export default Axios.create({
-    baseURL: "http://localhost:8080/api",
-    headers: {
-        'Content-type': 'application/json',
-        'X-Requested-With': 'XMLHttpRequest'
-    }
-});
